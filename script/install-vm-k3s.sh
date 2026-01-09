9f3b4639943fae5dbd95b328ede09d34232897fd
gaetan <gaetan.g237@gmail.com>
2026-01-09 15:43:56 +0100

    feat: setup.sh

--- script/install-vm-k3s.sh
+++ script/install-vm-k3s.sh
@@ -1,16 +1,83 @@
 #!/bin/bash
+set -euo pipefail
 
-# TODO
-# Tester qu'il n'existe pas déjà une VM et un disque du même nom
-# Tester que virt-install et kvm sont installé
+XML_PATH=".default-network.xml"
+VM_NAME="k3s"
+VOL_NAME="k3s.qcow2"
+POOL="default"
+
+sudo apt install -y \
+  qemu-kvm \
+  libvirt-daemon-system \
+  libvirt-clients \
+  virtinst \
+  virt-manager \
+  openssh-client
+
+if ! groups | grep -q "libvirt"; then
+  sudo usermod -aG libvirt $USER
+  newgrp libvirt
+fi
+
+
+
+# Vérifier que virbr0 existe
+if ! ip -d link show virbr0 2>/dev/null | grep -q "bridge"; then
+  echo "erreur: le bridge virbr0 n'existe pas" 1>&2
+  exit 1
+fi
+
+# vérifier que le réseau default existe bien
+if ! virsh net-info default &>/dev/null; then
+  cat > "$XML_PATH" <<'EOF'
+<network>
+  <name>default</name>
+  <forward mode='nat'/>
+  <bridge name='virbr0' stp='on' delay='0'/>
+  <mac address='52:54:00:58:e6:ee'/>
+  <ip address='192.168.122.1' netmask='255.255.255.0'>
+    <dhcp>
+      <range start='192.168.122.3' end='192.168.122.254'/>
+    </dhcp>
+  </ip>
+</network>
+EOF
+  virsh net-define "$XML_PATH"
+fi
+virsh net-start default &>/dev/null || true
+virsh net-autostart default
+
+
+
+# Vérifier si la VM existe
+if virsh dominfo "$VM_NAME" >/dev/null 2>&1; then
+    echo "Erreur : la VM '$VM_NAME' existe déjà." 1>&2
+    echo "Pour supprimer la VM '$VM_NAME' tapez : virsh destroy $VM_NAME && virsh undefine $VM_NAME"
+    exit 1
+fi
+
+# Vérifier si le volume existe
+if virsh vol-info --pool "$POOL" "$VOL_NAME" >/dev/null 2>&1; then
+    echo "Erreur : le volume '$VOL_NAME' existe déjà dans le pool '$POOL'." 1>&2
+    echo "Pour supprimer le volume '$VOL_NAME' tapez : virsh vol-delete $VM_NAME.qcow2 --pool $POOL"
+    exit 1
+fi
+
+# Générer une clé ssh
+mkdir -p ~/.ssh/kvm/
+rm -f ~/.ssh/kvm/id_ed25519 ~/.ssh/kvm/id_ed25519.pub
+ssh-keygen -t ed25519 -f ~/.ssh/kvm/id_ed25519 -N "" -C ""
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R 192.168.122.2 2>/dev/null || true
 
 # Temps d'installation (hors téléchargement) : 4-5mn
 echo "Téléchargement de l'image de la VM..."
 curl -o jammy-server-cloudimg-amd64.img https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
 sudo mv jammy-server-cloudimg-amd64.img /var/lib/libvirt/images/
+TMP_USERDATA="$(mktemp)"
+sed "s|__SSH_KEY__|$(cat ~/.ssh/kvm/id_ed25519.pub)|" "$(pwd)/infra/terraform/vm-k3s.yaml" > "$TMP_USERDATA"
 echo "installation de la VM..."
 virt-install \
-  --name k3s \
+  --name $VM_NAME \
   --memory 3072 \
   --vcpus 3 \
   --cpu host \
@@ -18,7 +85,7 @@ virt-install \
   --disk \
     size=10,backing_store="/var/lib/libvirt/images/jammy-server-cloudimg-amd64.img",bus=virtio \
   --cloud-init \
-    user-data="$(pwd)/infra/terraform/vm-k3s.yaml",network-config="$(pwd)/infra/terraform/network-config.yaml" \
+    user-data="$TMP_USERDATA",network-config="$(pwd)/infra/terraform/network-config.yaml" \
   --network \
     network=default,model=virtio \
   --noautoconsole
