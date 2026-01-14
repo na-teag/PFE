c1e636bf724ace003e95772fe2c7400f31bd7019
gaetan <gaetan.g237@gmail.com>
2026-01-14 19:15:47 +0100

    feat: better installation info

--- setup.sh
+++ setup.sh
@@ -4,10 +4,8 @@ set -euo pipefail
 URL="http://192.168.122.2:8000/"
 
 # Installer terraform si absent
-if terraform --version >/dev/null 2>&1; then
-    echo "Terraform est déjà installé"
-else
-    echo "Installation de Terraform..."
+if ! terraform --version >/dev/null 2>&1; then
+    echo -e "\n#################################\n### Installation de Terraform ###\n#################################"
     wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
     sudo apt update
@@ -33,6 +31,7 @@ fi
 
 
 # attendre que les services soient dispo
+echo -e "\n\n"
 while true; do
     STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$URL" || echo "000")
     if [[ "$STATUS" == "200" ]]; then
@@ -45,6 +44,7 @@ while true; do
 done
 
 # appliquer la clé VirusTotal via ssh
+echo
 read -s -p "Entrez votre clé VirusTotal API : " VT_KEY
 echo
 
@@ -60,6 +60,7 @@ echo ok
 '"
 
 # Afficher les informations de connexion à argocd
+echo -e "\n\n"
 ssh k3s@192.168.122.2 -i ~/.ssh/kvm/id_ed25519 'echo "service ArgoCD : https://$(hostname -I | awk "{print \$1}"):$(kubectl get svc argocd-server -n argocd -o jsonpath="{.spec.ports[?(@.port==443)].nodePort}")"; echo "id : admin"; echo "pwd : $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"'
 
 # afficher l'interface graphique du projet sur firefox
