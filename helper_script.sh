4ffac0709885f24dc61528d95a620c120547ef80
gaetan <gaetan.g237@gmail.com>
2026-01-18 03:25:58 +0100

    fix: remove offline files

--- helper_script.sh
+++
@@ -1,4 +0,0 @@
-echo -e "\n### Bringing up network bridge ###"
-sudo /home/cuckoo/vmcloak/bin/vmcloak-qemubridge br0 192.168.30.1/24
-echo -e "\n### Mounting ISO ###"
-sudo mount -o loop,ro /home/cuckoo/win10x64.iso /mnt/win10x64
