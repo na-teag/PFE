7dafb2afce2309c773d7a1e96d80b032ccbac35f
gaetan <gaetan.g237@gmail.com>
2026-04-23 17:38:48 +0200

    chore: move namespace creation in cloud-init, delete old code

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-VM_K3S="k3s.qcow2"
-IP="192.168.122.2"
+K3S_NAME="k3s"
+VM_K3S="$K3S_NAME.qcow2"
+IP_K3S="192.168.122.2"
 NAMESPACE="malware-analysis"
 SSH_KEY="$HOME/.ssh/kvm/id_ed25519"
-SSH_TARGET="k3s@${IP}"
+SSH_TARGET_K3S="$K3S_NAME@${IP_K3S}"
 CERT_DIR="./certs"
-URL="https://192.168.122.2/"
+URL="https://$IP_K3S/"
 
 # Ajouter les droits d'éxecution pour tout les scripts
 sudo chmod +x infra/cuckoo3/install.sh
@@ -31,34 +32,15 @@ echo "Merci de patienter jusqu'au démarrage complet des services sur la VM..."
 echo
 while true; do
     STATUS=$(curl -sk -o /dev/null -w "%{http_code}" "$URL" || echo "000")
-    if [[ "$STATUS" == "200" ]]; then
+    PODS=$(ssh -i "$SSH_KEY" "$SSH_TARGET_K3S" "kubectl get pods -n malware-analysis | wc -l")
+    if [[ "$STATUS" == "200" &&  "$PODS" == "8" ]]; then
         echo "Services disponibles."
         break
     else
-        echo "Services non disponible (HTTP $STATUS). Nouvelle tentative dans 10 secondes..."
+        echo -e "Services non disponible (réponse HTTP: $STATUS, pods démarrés : $PODS/8).\nNouvelle tentative dans 10 secondes..."
         sleep 10
     fi
 done
-
-# appliquer la clé VirusTotal via ssh
-#echo -e "\n\n"
-#read -s -p "Entrez votre clé VirusTotal API : " VT_KEY
-#echo
-
-# lire la clé Cuckoo API depuis un fichier local (sur ta machine)
-#CUCKOO_API_KEY="$(cat "$(pwd)/cuckoo_api_key.txt")"
-
-#générer la clé API
-#API_KEY=$(openssl rand -base64 32)
-
-#ssh-keyscan -H 192.168.122.2 >> "$HOME/.ssh/known_hosts"
-#ssh -i ~/.ssh/kvm/id_ed25519 k3s@192.168.122.2 "kubectl create namespace malware-analysis --dry-run=client -o yaml | kubectl apply -f -"
-#ssh -i ~/.ssh/kvm/id_ed25519 k3s@192.168.122.2 "VT_KEY='$VT_KEY' CUCKOO_KEY='$CUCKOO_API_KEY' API_KEY='$API_KEY' bash -c '
-#kubectl patch secret vt-credentials -n malware-analysis --type=merge -p \"{\\\"stringData\\\":{\\\"VIRUSTOTAL_API_KEY\\\":\\\"\$VT_KEY\\\",\\\"CUCKOO_API_KEY\\\":\\\"\$CUCKOO_KEY\\\",\\\"API_KEY\\\":\\\"\$API_KEY\\\"}}\"
-#kubectl delete pod -n malware-analysis -l app=worker-static
-#kubectl delete pod -n malware-analysis -l app=sandbox-controller
-#echo ok
-#'"
 
 
 ########################################
@@ -70,18 +52,15 @@ echo "=== Configuration des Secrets et Application ArgoCD ==="
 echo -e "\n"
 read -s -p "Entrez votre clé VirusTotal API : " VT_KEY
 echo
-# Vérification rapide pour éviter les variables vides
 if [ -z "$VT_KEY" ]; then echo "Erreur: La clé VT est vide."; exit 1; fi
 
 CUCKOO_API_KEY="$(cat "$(pwd)/cuckoo_api_key.txt")"
 API_KEY=$(openssl rand -base64 32)
 
-# Créer le namespace
-ssh -i "$SSH_KEY" "$SSH_TARGET" "kubectl create namespace malware-analysis --dry-run=client -o yaml | kubectl apply -f -"
 
 # Créer le secret vt-credentials
-# Le 'cat <<EOF' permet d'injecter tes variables locales directement dans le flux SSH
-ssh -i "$SSH_KEY" "$SSH_TARGET" "cat <<EOF | kubectl apply -f -
+ssh-keyscan -H $IP_K3S >> "$HOME/.ssh/known_hosts"
+ssh -i "$SSH_KEY" "$SSH_TARGET_K3S" "cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: Secret
 metadata:
@@ -94,13 +73,10 @@ stringData:
 type: Opaque
 EOF"
 
-# Appliquer l'application ArgoCD
-# On lit le fichier local et on le pousse via le pipe
-cat infra/argocd/malware-analysis-app.yaml | ssh -i "$SSH_KEY" "$SSH_TARGET" "kubectl apply -f - -n argocd"
 
 # Nettoyage des pods pour forcer le redémarrage avec les nouveaux secrets
-ssh -i "$SSH_KEY" "$SSH_TARGET" "kubectl delete pod -n malware-analysis -l app=worker-static --ignore-not-found"
-ssh -i "$SSH_KEY" "$SSH_TARGET" "kubectl delete pod -n malware-analysis -l app=sandbox-controller --ignore-not-found"
+ssh -i "$SSH_KEY" "$SSH_TARGET_K3S" "kubectl delete pod -n malware-analysis -l app=worker-static"
+ssh -i "$SSH_KEY" "$SSH_TARGET_K3S" "kubectl delete pod -n malware-analysis -l app=sandbox-controller"
 
 
 ########################################
@@ -114,14 +90,14 @@ if [[ ! -f "$CERT_DIR/tls.crt" || ! -f "$CERT_DIR/tls.key" ]]; then
     -keyout "${CERT_DIR}/tls.key" \
     -out    "${CERT_DIR}/tls.crt" \
     -days   365 \
-    -subj   "/CN=${IP}/O=MalwareAnalysis" \
-    -addext "subjectAltName=IP:${IP}"
+    -subj   "/CN=${IP_K3S}/O=MalwareAnalysis" \
+    -addext "subjectAltName=IP_K3S:${IP_K3S}"
 fi
 
 # 2. Pousser le Secret TLS sur K3s
 echo "=== Déploiement du Secret TLS ==="
-scp -i "$SSH_KEY" "${CERT_DIR}/tls.crt" "${CERT_DIR}/tls.key" "${SSH_TARGET}:/tmp/"
-ssh -i "$SSH_KEY" "$SSH_TARGET" "
+scp -i "$SSH_KEY" "${CERT_DIR}/tls.crt" "${CERT_DIR}/tls.key" "${SSH_TARGET_K3S}:/tmp/"
+ssh -i "$SSH_KEY" "$SSH_TARGET_K3S" "
   kubectl create secret tls api-tls \
     --cert=/tmp/tls.crt \
     --key=/tmp/tls.key \
@@ -132,7 +108,7 @@ ssh -i "$SSH_KEY" "$SSH_TARGET" "
 
 
 echo "=== Configuration de Traefik sur le port 443 ==="
-ssh -i "$SSH_KEY" "$SSH_TARGET" '
+ssh -i "$SSH_KEY" "$SSH_TARGET_K3S" '
   TRAEFIK_SVC=$(kubectl get svc -n kube-system -l app.kubernetes.io/name=traefik -o name | head -1)
   kubectl patch "$TRAEFIK_SVC" -n kube-system --type=merge -p '"'"'{
     "spec": {
@@ -146,11 +122,8 @@ ssh -i "$SSH_KEY" "$SSH_TARGET" '
   echo "Traefik sur port 443"
   '
 
-ssh -i ~/.ssh/kvm/id_ed25519 k3s@192.168.122.2 \
-  "kubectl patch svc argocd-server -n argocd --type=merge -p '{\"spec\":{\"type\":\"NodePort\"}}'"
-
 # On crée un service pour forcer le bon redéploiement d'argocd à chaque redémarrage de la vm
-cat << 'EOF' | ssh -i ~/.ssh/kvm/id_ed25519 k3s@192.168.122.2 'cat > /tmp/fix-argocd.sh'
+cat << 'EOF' | ssh -i ~/.ssh/kvm/id_ed25519 $SSH_TARGET_K3S 'cat > /tmp/fix-argocd.sh'
 cat > /etc/systemd/system/fix-argocd.service << 'UNIT'
 [Unit]
 Description=Fix argocd-repo-server after boot
@@ -172,11 +145,11 @@ systemctl enable fix-argocd.service
 echo "Service créé et activé."
 EOF
 
-ssh -t -i ~/.ssh/kvm/id_ed25519 k3s@192.168.122.2 'sudo bash /tmp/fix-argocd.sh && rm /tmp/fix-argocd.sh'
+ssh -t -i ~/.ssh/kvm/id_ed25519 $SSH_TARGET_K3S 'sudo bash /tmp/fix-argocd.sh && rm /tmp/fix-argocd.sh'
 
 # Afficher les informations de connexion à argocd
 echo -e "\n\n"
-ssh k3s@192.168.122.2 -i ~/.ssh/kvm/id_ed25519 'echo "service ArgoCD : https://$(hostname -I | awk "{print \$1}"):$(kubectl get svc argocd-server -n argocd -o jsonpath="{.spec.ports[?(@.port==443)].nodePort}")"; echo "id : admin"; echo "pwd : $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"'
+ssh $SSH_TARGET_K3S -i ~/.ssh/kvm/id_ed25519 'echo "service ArgoCD : https://$(hostname -I | awk "{print \$1}"):$(kubectl get svc argocd-server -n argocd -o jsonpath="{.spec.ports[?(@.port==443)].nodePort}")"; echo "id : admin"; echo "pwd : $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"'
 
 # Afficher la clé API
 echo "API KEY: $API_KEY"
